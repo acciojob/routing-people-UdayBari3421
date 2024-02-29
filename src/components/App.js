7d3e9e8c27a5230f9d42223788b4d9860be3155c
Uday Bari <115573575+UdayBari3421@users.noreply.github.com>
2024-02-29 05:54:27 +0000

    submitting code-Thu Feb 29 2024 05:54:25 GMT+0000 (Coordinated Universal Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserDetails from "./UserDetails";
+import UserList from "./UserList";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<UserList />} />
+          <Route path="/users/:id" element={<UserDetails />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
