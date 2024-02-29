cdca24ae83ba19ee486e1729ea0553fc6adb2b0c
Uday Bari <115573575+UdayBari3421@users.noreply.github.com>
2024-02-29 05:57:04 +0000

    submitting code-Thu Feb 29 2024 05:57:02 GMT+0000 (Coordinated Universal Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,17 @@
 import React from "react";
-import "./../styles/App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import UserDetails from "./UserDetails";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserList from "./UserList";
+import UserDetails from "./UserDetails";
 
-const App = () => {
+function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<UserList />} />
-          <Route path="/users/:id" element={<UserDetails />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<UserList />} />
+        <Route path="/users/:id" element={<UserDetails />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
